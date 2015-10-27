b121b25e40133b56e763d0e916b5df65a95a12dd
Sergey Shushlyapin <ss@sitecore.net>
2015-10-27 18:34:58 +0200

    Simplify adding a DbItem to a Db and getting the Item instance back out of the FakeDb (#98)

---
+++ src/Sitecore.FakeDb.Tests/DbExtensionsTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Sitecore.FakeDb.Tests
+{
+  using FluentAssertions;
+  using Sitecore.Data.Items;
+  using Xunit;
+
+  public class DbExtensionsTest
+  {
+    [Fact]
+    public void ShouldCreateAndGetItem()
+    {
+      using (var db = new Db())
+      {
+        db.Create(new DbItem("item")).Should().BeAssignableTo<Item>();
+      }
+    }
+
+    [Fact]
+    public void ShouldCreateAndGetTemplate()
+    {
+      using (var db = new Db())
+      {
+        db.Create(new DbTemplate("template")).Should().BeAssignableTo<TemplateItem>();
+      }
+    }
+  }
+}
---
+++ src/Sitecore.FakeDb/DbExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Sitecore.FakeDb
+{
+  using Sitecore.Data.Items;
+
+  public static class DbExtensions
+  {
+    /// <summary>
+    /// Adds a new <see cref="DbItem"/> to the database and returns the <see cref="Item"/> instance.
+    /// </summary>
+    /// <param name="db">The db context.</param>
+    /// <param name="item">The item to create.</param>
+    /// <returns>The <see cref="Item"/> instance.</returns>
+    public static Item Create(this Db db, DbItem item)
+    {
+      db.Add(item);
+      return db.GetItem(item.ID);
+    }
+
+    /// <summary>
+    /// Adds a new <see cref="DbTemplate"/> to the database and returns the <see cref="TemplateItem"/> instance.
+    /// </summary>
+    /// <param name="db">The db context.</param>
+    /// <param name="template">The template to create.</param>
+    /// <returns>The <see cref="TemplateItem"/> instance.</returns>
+    public static TemplateItem Create(this Db db, DbTemplate template)
+    {
+      db.Add(template);
+      return db.GetItem(template.ID);
+    }
+  }
+}
